7908d7e7423660ac008ab4305f1595a1b2121eb6
DLM <55211307+dlminvestments@users.noreply.github.com>
2021-10-23 19:58:35 -0400

    Create socket.js

---
+++ socket io/socket.js
@@ -0,0 +1,5 @@
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+io.on('connection', () => { /* … */ });
+server.listen(7777);
