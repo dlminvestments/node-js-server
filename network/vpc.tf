5e5d81c6d0f300d799eced6b7eafd287e80fbefe
DLM <55211307+dlminvestments@users.noreply.github.com>
2022-01-13 00:58:36 -0500

    Update vpc.tf

--- network/vpc.tf
+++ network/vpc.tf
@@ -11,7 +11,7 @@ module "vpc" {
   cidr = "172.31.0.0/16"
 
   azs                = [data.aws_availability_zones.available.names[0], data.aws_availability_zones.available.names[1], data.aws_availability_zones.available.names[2]]
-  public_subnets     = ["172.31.0.0/20", "172.31.0.0/20", "172.31.32.0/20"]
+  public_subnets     = ["172.31.0.0/20", "172.31.48.0/20", "172.31.32.0/20"]
   private_subnets    = ["172.31.16.0/20”, "172.31.80.0/20", "172.31.64.0/20"]
   enable_nat_gateway = true
   single_nat_gateway = true
